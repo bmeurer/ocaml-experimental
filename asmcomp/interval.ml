7a35e633c4d0e533a78ea45b84ca4210c39e0deb
Benedikt Meurer <benedikt.meurer@googlemail.com>
2011-09-26 19:31:45 +0200

    Adjust code from the last commit.

--- asmcomp/interval.ml
+++ asmcomp/interval.ml
@@ -33,10 +33,10 @@ type t =
     mutable ranges: range list;
   }
 
-type operand_type =
-    OTResult
-  | OTArgument
-  | OTLive
+type kind =
+    Result
+  | Argument
+  | Live
 
 let interval_list = ref ([] : t list)
 let fixed_interval_list = ref ([] : t list)
@@ -71,59 +71,45 @@ let remove_expired_ranges i pos =
                else filter rl' in
   i.ranges <- filter i.ranges
 
-let update_interval_ranges interval range on off operand_type =
-  let b = match operand_type with
-            OTResult -> off
-          | OTArgument | OTLive -> on in
-
-  let e = match operand_type with
-            OTResult | OTLive -> off
-          | OTArgument -> on in
-
-  interval.iend <- e;
-  if range.rend = on - 1 || range.rend = on - 2 then
-    range.rend <- e
-  else
-    interval.ranges <- {rbegin=b; rend=e} :: interval.ranges
-
-let update_interval_position intervals pos operand_type reg =
+let update_interval_position intervals pos kind reg =
   let i = intervals.(reg.stamp) in
   let on = pos lsl 1 in
   let off = on + 1 in
-  if i.iend = 0 then begin
+  let rbegin = (match kind with Result -> off | _ -> on) in
+  let rend = (match kind with Argument -> on | _ -> off) in
+  if i.iend == 0 then begin
     i.ibegin <- off;
-    i.iend <- off;
     i.reg <- reg;
-    update_interval_ranges i {rbegin=(-100); rend=(-100)} on off operand_type
-  end
-  else begin
-    match i.ranges with
-      [] ->
-        Misc.fatal_error "Illegal empty range"
-    | range :: _ ->
-        update_interval_ranges i range on off operand_type
-  end
+    i.ranges <- {rbegin = rbegin; rend = rend} :: i.ranges
+  end else begin
+    let r = List.hd i.ranges in
+    if r.rend == on - 1 || r.rend == on - 2 then
+      r.rend <- rend
+    else
+      i.ranges <- {rbegin = rbegin; rend = rend} :: i.ranges
+  end;
+  i.iend <- rend
 
-let update_interval_position_by_array intervals regs pos operand_type=
-  Array.iter (update_interval_position intervals pos operand_type) regs
+let update_interval_position_by_array intervals regs pos kind =
+  Array.iter (update_interval_position intervals pos kind) regs
          
-let update_interval_position_by_set intervals regs pos operand_type=
-  Set.iter (update_interval_position intervals pos operand_type) regs
+let update_interval_position_by_set intervals regs pos kind =
+  Set.iter (update_interval_position intervals pos kind) regs
  
 let update_interval_position_by_instr intervals instr pos =
-  update_interval_position_by_array intervals instr.arg pos OTArgument;
-  update_interval_position_by_array intervals instr.res pos OTResult;
-  update_interval_position_by_set intervals instr.live pos OTLive
+  update_interval_position_by_array intervals instr.arg pos Argument;
+  update_interval_position_by_array intervals instr.res pos Result;
+  update_interval_position_by_set intervals instr.live pos Live
 
 let insert_destroyed_at_oper intervals instr pos =
   let destroyed = Proc.destroyed_at_oper instr.desc in
   if Array.length destroyed > 0 then
-    update_interval_position_by_array intervals destroyed pos OTResult
+    update_interval_position_by_array intervals destroyed pos Result
 
 let insert_destroyed_at_raise intervals pos =
   let destroyed = Proc.destroyed_at_raise in
   if Array.length destroyed > 0 then
-    update_interval_position_by_array intervals destroyed pos OTResult
+    update_interval_position_by_array intervals destroyed pos Result
 
 (* Build all intervals.
    The intervals will be expanded by one step at the start and end
@@ -139,44 +125,44 @@ let build_intervals fd =
                       ranges = []; }) in
   let pos = ref 0 in
   let rec walk_instruction i =
-    pos := !pos + 1;
-    update_interval_position_by_instr intervals i !pos;
+    let pos = incr pos; !pos in
+    update_interval_position_by_instr intervals i pos;
     begin match i.desc with
       Iend -> ()
     | Iop(Icall_ind | Icall_imm _ | Iextcall(_, true)
           | Itailcall_ind | Itailcall_imm _) ->
         walk_instruction i.next
     | Iop _ ->
-        insert_destroyed_at_oper intervals i !pos;
+        insert_destroyed_at_oper intervals i pos;
         walk_instruction i.next
     | Ireturn ->
-        insert_destroyed_at_oper intervals i !pos;
+        insert_destroyed_at_oper intervals i pos;
         walk_instruction i.next
     | Iifthenelse(_, ifso, ifnot) ->
-        insert_destroyed_at_oper intervals i !pos;
+        insert_destroyed_at_oper intervals i pos;
         walk_instruction ifso;
         walk_instruction ifnot;
         walk_instruction i.next
     | Iswitch(_, cases) ->
-        insert_destroyed_at_oper intervals i !pos;
-        Array.iter (fun case -> walk_instruction case) cases;
+        insert_destroyed_at_oper intervals i pos;
+        Array.iter walk_instruction cases;
         walk_instruction i.next
     | Iloop body ->
-        insert_destroyed_at_oper intervals i !pos;
+        insert_destroyed_at_oper intervals i pos;
         walk_instruction body;
         walk_instruction i.next
     | Icatch(_, body, handler) ->
-        insert_destroyed_at_oper intervals i !pos;
+        insert_destroyed_at_oper intervals i pos;
         walk_instruction body;
         walk_instruction handler;
         walk_instruction i.next
     | Iexit _ ->
-        insert_destroyed_at_oper intervals i !pos;
+        insert_destroyed_at_oper intervals i pos;
         walk_instruction i.next
     | Itrywith(body, handler) ->
-        insert_destroyed_at_oper intervals i !pos;
+        insert_destroyed_at_oper intervals i pos;
         walk_instruction body;
-        insert_destroyed_at_raise intervals !pos;
+        insert_destroyed_at_raise intervals pos;
         walk_instruction handler;
         walk_instruction i.next
     | Iraise ->
